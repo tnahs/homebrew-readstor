06ff28e1028016574d2c56407c5f547a2dfb1d38
tnahs <31777797+tnahs@users.noreply.github.com>
2022-01-31 09:43:58 -0800

    Bump to v0.2.0

--- Formula/readstor.rb
+++ Formula/readstor.rb
@@ -1,16 +1,10 @@
-# References:
-#    https://docs.brew.sh/Formula-Cookbook
-#    https://rubydoc.brew.sh/Formula
-#    https://skerritt.blog/packaging-your-rust-package/
-#    https://federicoterzi.com/blog/how-to-publish-your-rust-project-on-homebrew/
-#    https://rubydoc.brew.sh/Formula
 class Readstor < Formula
   desc "A CLI for Apple Books annotations"
   homepage "https://github.com/tnahs/readstor"
-  url "https://github.com/tnahs/readstor/releases/download/v0.1.2/readstor-mac.tar.gz"
-  sha256 "683fba87842c0330ab1a316bc32cb8ad407f67b2924281acba55e59b102d8def"
-  license "MIT"
-  version "0.1.2"
+  url "https://github.com/tnahs/readstor/releases/download/v0.2.0/readstor-mac.tar.gz"
+  sha256 "6c4a188145fdce19292edbdba255631f7b748345983350a4cb2d675171d6e554"
+  license any_of: ["MIT", "Apache-2.0"]
+  version "0.2.0"
 
   def install
     bin.install "readstor"
---
+++ REFERENCES.md
@@ -0,0 +1,7 @@
+# References:
+
+- <https://docs.brew.sh/Formula-Cookbook>
+- <https://rubydoc.brew.sh/Formula>
+- <https://skerritt.blog/packaging-your-rust-package/>
+- <https://federicoterzi.com/blog/how-to-publish-your-rust-project-on-homebrew/>
+- <https://rubydoc.brew.sh/Formula>
